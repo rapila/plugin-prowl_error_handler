b445b3f7e1a276848c4434a8b245bca24cb3f72c
Raphael Schweikert <rs@rapi.la>
2013-06-11 14:45:31 +0200

    Update ProwlFilterModule.php
    Don’t send prowl notifications on user errors.

--- modules/filter/prowl/ProwlFilterModule.php
+++ modules/filter/prowl/ProwlFilterModule.php
@@ -1,7 +1,10 @@
 <?php
 class ProwlFilterModule extends FilterModule {
-  public function onAnyError($aOptions) {
-    $aProwlConfig = Settings::getSetting('prowl', 'users', array());
+	public function onAnyError($aOptions, $bNeverPrint = false, $bNeverNotifyDeveloper = false) {
+		if($bNeverNotifyDeveloper) {
+			return;
+		}
+		$aProwlConfig = Settings::getSetting('prowl', 'users', array());
 		$aKeys = array();
 		foreach($aProwlConfig as $aConfig) {
 			if(in_array(ErrorHandler::getEnvironment(), $aConfig['environments'])) {
@@ -11,19 +14,19 @@ class ProwlFilterModule extends FilterModule {
 		if(count($aKeys) == 0) {
 			return;
 		}
-    $aError = &$aOptions[0];
+		$aError = &$aOptions[0];
 		
-    $aProwlParams = array();
-    $aProwlParams['apikey'] = implode(',', $aKeys);
-    $aProwlParams['application'] = "Rapila on ".$aError['host'];
-    $aProwlParams['event'] = "Error on in ".$aError['path'];
-    $aProwlParams['url'] = 'http://'.$aError['host'].$aError['path'];
-    $aProwlParams['description'] = StringUtil::truncate($aError['message'], 800);
+		$aProwlParams = array();
+		$aProwlParams['apikey'] = implode(',', $aKeys);
+		$aProwlParams['application'] = "Rapila on ".$aError['host'];
+		$aProwlParams['event'] = "Error on in ".$aError['path'];
+		$aProwlParams['url'] = 'http://'.$aError['host'].$aError['path'];
+		$aProwlParams['description'] = StringUtil::truncate($aError['message'], 800);
 		
-    $sParams = http_build_query($aProwlParams);
+		$sParams = http_build_query($aProwlParams);
 		$rCurl = curl_init('https://api.prowlapp.com/publicapi/add');
 		curl_setopt($rCurl, CURLOPT_POSTFIELDS, $sParams);
 		curl_setopt($rCurl, CURLOPT_POST, 1);
 		curl_exec($rCurl);
-  }
+	}
 }
